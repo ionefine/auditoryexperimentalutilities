655add6271ecb54f8fcbe4707d9afcd9cc9b91b1
Ione Fine <ionefine@uw.edu>
2026-03-02 12:32:58 -0800

    Clean up staircase analysis and harden loudness matching inputs

--- StaircaseAnalysis.m
+++ StaircaseAnalysis.m
@@ -1,143 +1,88 @@
-%StaircaseAnalysis.m
+% StaircaseAnalysis.m
 %
-% Exploration and anlysis of loudness matching results
+% Exploration and analysis of loudness-matching results.
 
-% load in a 'results' structure
-load result3
-nFreqs = length(result.perFreq);
-
-% Find the maxumum number of trials across the staircases, used to make the
-% staircase plots have the same aspect ratio since some staircaises have
-% more trials than others.
-
-maxx = 0;
-for i=1:nFreqs
-    maxx = max(maxx,length(result.perFreq(i).hist.choice));
+% Load a MAT file that contains either `result` or `results`.
+S = load('result3.mat');
+if isfield(S, 'result')
+    result = S.result;
+elseif isfield(S, 'results')
+    result = S.results;
+else
+    error('Expected variable `result` or `results` in result3.mat.');
 end
 
-%%
-% Show the staircases in figure 1, and the psychometric functions in figure
-% 2 along with the ML fits with the cumulative normal.
+nFreqs = numel(result.perFreq);
 
-figure(1);clf
-figure(2);clf
+% Find the maximum number of trials across staircases so axes align.
+maxx = 0;
+for i = 1:nFreqs
+    maxx = max(maxx, numel(result.perFreq(i).hist.choice));
+end
 
-for i=1:nFreqs
+% Show staircases in figure 1 and psychometric fits in figure 2.
+figure(1); clf;
+figure(2); clf;
 
+reversal_thresh = zeros(1, nFreqs);
+psycho_thresh = zeros(1, nFreqs);
+freq = zeros(1, nFreqs);
+
+for i = 1:nFreqs
     y = result.perFreq(i).hist.amp_present;
     x = 1:numel(y);
-    figure(1)
-    subplot(1,nFreqs,i)
-    stairs(y)
 
-    down = result.perFreq(i).hist.targetLouder ==1;
+    figure(1);
+    subplot(1, nFreqs, i);
+    stairs(y);
 
-    hold on
+    down = result.perFreq(i).hist.targetLouder == 1;
 
-    plot(x(down),y(down),'bo','MarkerFaceColor','g','MarkerSize',4)
-    plot(x(~down),y(~down),'bo','MarkerFaceColor','r','MarkerSize',4)
+    hold on;
+    plot(x(down), y(down), 'bo', 'MarkerFaceColor', 'g', 'MarkerSize', 4);
+    plot(x(~down), y(~down), 'bo', 'MarkerFaceColor', 'r', 'MarkerSize', 4);
 
-    title(sprintf('%g Hz',result.perFreq(i).freqHz))
-    set(gca,'YLim',[0,.6])
-    set(gca,'XLim',[0,maxx+1])
-    plot([0,maxx+1],result.perFreq(i).estTargetAmp*[1,1],'k-','LineWidth',1)
-    grid
+    title(sprintf('%g Hz', result.perFreq(i).freqHz));
+    set(gca, 'YLim', [0, .6]);
+    set(gca, 'XLim', [0, maxx + 1]);
+    plot([0, maxx + 1], result.perFreq(i).estTargetAmp * [1, 1], 'k-', 'LineWidth', 1);
+    grid on;
 
-    foo.intensity = round(y,4);
+    foo.intensity = round(y, 4);
     foo.response = down;
 
-    figure(2)
-    
-    subplot(1,nFreqs,i)
-    
-    % initial conditions for the cumulative normal.  t = mean, b = sd.
+    figure(2);
+    subplot(1, nFreqs, i);
+
+    % Initial conditions for cumulative normal. t = mean, b = sd.
     p.t = .3;
     p.b = .1;
-    p = fit('fitPsychometricFunction',p,{'t','b'},foo,'Normal');
-        err = fitPsychometricFunction(p,foo,'Normal');
+    p = fit('fitPsychometricFunction', p, {'t', 'b'}, foo, 'Normal');
+    err = fitPsychometricFunction(p, foo, 'Normal'); %#ok<NASGU>
 
-    plotPsycho(foo,'Amplitude',p,'Normal')
-    figure(1)
+    plotPsycho(foo, 'Amplitude', p, 'Normal');
 
-    plot([0,maxx+1],p.t*[1,1],'m-','LineWidth',3)
-    grid
+    figure(1);
+    plot([0, maxx + 1], p.t * [1, 1], 'm-', 'LineWidth', 3);
+    grid on;
 
-    % save the matches defined by reversals and normal fits.
+    % Save the matches defined by reversals and normal fits.
     reversal_thresh(i) = result.perFreq(i).estTargetAmp;
     psycho_thresh(i) = p.t;
     freq(i) = result.perFreq(i).freqHz;
 end
 
-%%
-%
-% Plot bar graphs  
-figure(3)
-clf
-hold on
-h(1) = plot(log(freq),reversal_thresh,'ko-','MarkerFaceColor','b');
-h(2) = plot(log(freq),psycho_thresh,'ko-','MarkerFaceColor','m');
-h(3) = plot(log(freq),result.refAmp*ones(size(freq)),'k:');
-legend({'reversal','psycho','reference'})
-grid
-set(gca,'XTick',log(freq));
-set(gca,'YLim',[0,.75])
-logx2raw
-xlabel('Test Frequency (Hz)')
-ylabel(sprintf('Amplitude Matching %g Hz',result.refFreqHz));
-
-
-
-
-function plotPsycho(results,intensityName,p,functionName)
-% plotPsycho(results,intensityName,p,functionName)
-
-if ~exist('intensityName','var')
-    intensityName= 'Intensity';
-end
-
-intensities = unique(results.intensity);
-
-% Then we'll loop through these intensities calculating the proportion of
-% times that 'response' is equal to 1:
-
-nCorrect = zeros(1,length(intensities));
-nTrials = zeros(1,length(intensities));
-
-for i=1:length(intensities)
-    id = results.intensity == intensities(i) & isreal(results.response);
-    nTrials(i) = sum(id);
-    nCorrect(i) = sum(results.response(id));
-end
-
-pCorrect = nCorrect./nTrials;
-
-hold on
-
-sd = pCorrect.*(1-pCorrect)./sqrt(nTrials);  %pq/sqrt(n)
-errorbar((intensities),100*pCorrect,100*sd,'bo','MarkerFaceColor','b');
-
-if exist('p','var')
-    %plot the parametric psychometric function
-    x = linspace(min(results.intensity),max(results.intensity),101);
-    evalStr = sprintf('y=%s(p,x);',functionName);
-    eval(evalStr)
-    plot((x),100*y,'r-','LineWidth',2);
-
-end
-
-ylim  = get(gca,'YLim');
-xlim = get(gca,'XLim');
-
-pThresh = 100*(1/2)^(1/3);
-pThresh = 50;
-if exist('p','var')
-    plot([xlim(1),(p.t),(p.t)],[pThresh,pThresh,ylim(1)],'k-');
-    title(sprintf('Amplitude: %5.2g',p.t));
-end
-
-set(gca,'XTick',(intensities));
-%logx2raw
-%set(gca,'YLim',[0,100]);
-xlabel(intensityName);
-ylabel('Percent Louder');
-end
+% Plot summary curves.
+figure(3);
+clf;
+hold on;
+h(1) = plot(log(freq), reversal_thresh, 'ko-', 'MarkerFaceColor', 'b'); %#ok<NASGU>
+h(2) = plot(log(freq), psycho_thresh, 'ko-', 'MarkerFaceColor', 'm'); %#ok<NASGU>
+h(3) = plot(log(freq), result.refAmp * ones(size(freq)), 'k:'); %#ok<NASGU>
+legend({'reversal', 'psycho', 'reference'});
+grid on;
+set(gca, 'XTick', log(freq));
+set(gca, 'YLim', [0, .75]);
+logx2raw;
+xlabel('Test Frequency (Hz)');
+ylabel(sprintf('Amplitude Matching %g Hz', result.refFreqHz));
--- plotPsycho.m
+++ plotPsycho.m
@@ -1,66 +1,46 @@
-function plotPsycho(results,intensityName,p,functionName)
-% plotPsycho(results,intensityName,p,functionName)
+function plotPsycho(results, intensityName, p, functionName)
+% plotPsycho(results, intensityName, p, functionName)
 
-if ~exist('intensityName','var')
-    intensityName= 'Intensity';
+if ~exist('intensityName', 'var') || isempty(intensityName)
+    intensityName = 'Intensity';
 end
 
 intensities = unique(results.intensity);
 
-% Then we'll loop through these intensities calculating the proportion of
-% times that 'response' is equal to 1:
+% Loop through intensities calculating response=1 proportion.
+nCorrect = zeros(1, length(intensities));
+nTrials = zeros(1, length(intensities));
 
-nCorrect = zeros(1,length(intensities));
-nTrials = zeros(1,length(intensities));
-
-for i=1:length(intensities)
+for i = 1:length(intensities)
     id = results.intensity == intensities(i) & isreal(results.response);
     nTrials(i) = sum(id);
     nCorrect(i) = sum(results.response(id));
 end
 
-pCorrect = nCorrect./nTrials;
+pCorrect = nCorrect ./ nTrials;
 
+hold on;
+sd = pCorrect .* (1 - pCorrect) ./ sqrt(nTrials); % pq/sqrt(n)
+errorbar(log(intensities), 100 * pCorrect, 100 * sd, 'bo', 'MarkerFaceColor', 'b');
 
-
-
- hold on
- 
- sd = pCorrect.*(1-pCorrect)./sqrt(nTrials);  %pq/sqrt(n)
- errorbar(log(intensities),100*pCorrect,100*sd,'bo','MarkerFaceColor','b');
-
-
-if exist('p','var')
-    %plot the parametric psychometric function 
-    x = linspace(min(results.intensity),max(results.intensity),101);
-    evalStr = sprintf('y=%s(p,x);',functionName);
-    eval(evalStr)
-    plot(log(x),100*y,'r-','LineWidth',2);
-
+if exist('p', 'var') && ~isempty(p)
+    % Plot the parametric psychometric function.
+    x = linspace(min(results.intensity), max(results.intensity), 101);
+    y = feval(functionName, p, x);
+    plot(log(x), 100 * y, 'r-', 'LineWidth', 2);
 end
 
+ylimVals = get(gca, 'YLim');
+xlimVals = get(gca, 'XLim');
 
-ylim  = get(gca,'YLim');
-xlim = get(gca,'XLim');
+pThresh = 100 * (1 / 2)^(1 / 3);
 
-pThresh = 100*(1/2)^(1/3);  
-
-if exist('p','var')
-    plot([xlim(1),log(p.t),log(p.t)],[pThresh,pThresh,ylim(1)],'k-');
-    title(sprintf('Threshold: %5.2g',p.t));
+if exist('p', 'var') && ~isempty(p)
+    plot([xlimVals(1), log(p.t), log(p.t)], [pThresh, pThresh, ylimVals(1)], 'k-');
+    title(sprintf('Threshold: %5.2g', p.t));
 end
 
-set(gca,'XTick',log(intensities));
-logx2raw
-%set(gca,'YLim',[0,100]);
-xlabel('Coherence');
+set(gca, 'XTick', log(intensities));
+logx2raw;
+xlabel(intensityName);
 ylabel('Percent Correct');
-
-
-
-
-
-
-
-
-
